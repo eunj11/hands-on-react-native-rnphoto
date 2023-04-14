84f4d4713e6a8c1970e7130347fff9e251898aba
eunj11 <129266004+eunj11@users.noreply.github.com>
2023-04-14 21:15:24 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,3 +1,16 @@
-import App from './src/App';
+import { StatusBar } from 'expo-status-bar';
+import { LogBox } from 'react-native';
+import Navigation from './navigations/Navigation';
+import { UserProvider } from './contexts/UserContext';
+
+const App = () => {
+  LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core']);
+  return (
+    <UserProvider>
+      <StatusBar style="dark" />
+      <Navigation />
+    </UserProvider>
+  );
+};
 
 export default App;
