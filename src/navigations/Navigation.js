937c2bb219e1cca299a6abeefe86ed517a77ff3f
eunj11 <129266004+eunj11@users.noreply.github.com>
2023-04-14 21:24:23 +0900

    Add files via upload

---
+++ src/navigations/Navigation.js
@@ -0,0 +1,56 @@
+import { NavigationContainer } from '@react-navigation/native';
+import AuthStack from './AuthStack';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+import { Asset } from 'expo-asset';
+import { initFirebase } from '../api/firebase';
+import { useUserState } from '../contexts/UserContext';
+import MainStack from './MainStack';
+import { onAuthStateChanged } from '../api/auth';
+import ContentTab from './ContentTab';
+
+const Navigation = () => {
+  const [user, setUser] = useUserState();
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Asset.fromModule(
+          require('../../assets/cover.png')
+        ).downloadAsync();
+        const app = initFirebase();
+        const unsubscribe = onAuthStateChanged((user) => {
+          if (user) {
+            setUser(user);
+          }
+          setIsReady(true);
+          unsubscribe();
+        });
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e);
+        setIsReady(true);
+      }
+    })();
+  }, [setUser]);
+
+  const onReady = async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  };
+
+  if (!isReady) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer onReady={onReady}>
+      {user.uid ? <ContentTab /> : <AuthStack />}
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
---
+++ src/navigations/routes.js
@@ -0,0 +1,15 @@
+export const AuthRoutes = {
+  SIGN_IN: 'SignIn',
+  SIGN_UP: 'SignUp',
+};
+
+export const MainRoutes = {
+  PROFILE: 'Profile',
+};
+
+export const ContentRoutes = {
+  HOME: 'Home',
+  LIST: 'List',
+  MAP: 'Map',
+  PROFILE: 'Profile',
+};
