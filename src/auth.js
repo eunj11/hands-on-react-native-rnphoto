2dea88c5a2e98bf42ae9f5b9aa901c8ac8bd8d89
eunj11 <129266004+eunj11@users.noreply.github.com>
2023-04-14 21:20:08 +0900

    Add files via upload

---
+++ src/auth.js
@@ -0,0 +1,42 @@
+import {
+  AuthErrorCodes,
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged as onAuthStateChangedFirebase,
+} from 'firebase/auth';
+
+export const getAuthErrorMessages = (errorCode) => {
+  switch (errorCode) {
+    case AuthErrorCodes.USER_DELETED:
+      return '계정을 찾을 수 없습니다.';
+    case AuthErrorCodes.INVALID_EMAIL:
+      return '유효하지 않은 이메일 주소입니다.';
+    case AuthErrorCodes.INVALID_PASSWORD:
+      return '잘못된 비밀번호입니다.';
+    case AuthErrorCodes.EMAIL_EXISTS:
+      return '이미 가입된 이메일입니다.';
+    case AuthErrorCodes.WEAK_PASSWORD:
+      return '비밀번호는 최소 6자리 입니다.';
+    default:
+      return '로그인에 실패했습니다.';
+  }
+};
+
+export const signIn = async ({ email, password }) => {
+  const { user } = await signInWithEmailAndPassword(getAuth(), email, password);
+  return user;
+};
+
+export const signUp = async ({ email, password }) => {
+  const { user } = await createUserWithEmailAndPassword(
+    getAuth(),
+    email,
+    password
+  );
+  return user;
+};
+
+export const onAuthStateChanged = (callback) => {
+  return onAuthStateChangedFirebase(getAuth(), callback);
+};
