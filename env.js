d2a44e4903040ab7ab984567eaef2cddebdf41b1
eunj11 <129266004+eunj11@users.noreply.github.com>
2023-04-14 21:15:00 +0900

    Add files via upload

---
+++ env.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCTDh8nG2eTtKNyLawE0RDMHjs2liG288w',
+  authDomain: 'rnphoto-c27e0.firebaseapp.com',
+  projectId: 'rnphoto-c27e0',
+  storageBucket: 'rnphoto-c27e0.appspot.com',
+  messagingSenderId: '930549495003',
+  appId: '1:930549495003:web:75e14e04c566bfe08488fe',
+};
